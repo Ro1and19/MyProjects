b3ee9f983563394d491efccaf171847c148f4b0f
Ro1and19 <leonard6090@gmail.com>
2025-05-16 10:54:10 +0300

    upgraded

--- QRCode Service/src/main/java/org/example/Controller.java
+++ QRCode Service/src/main/java/org/example/Controller.java
@@ -1,33 +1,48 @@
 package org.example;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
 public class Controller {
-
     @GetMapping("/api/health")
     public String returnOK() {
         return "200 OK";
     }
 
     @GetMapping("/api/qrcode")
-    public ResponseEntity<BufferedImage> returnIMAGE() {
-        BufferedImage image = createImage(Color.BLUE);
-        return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(image);
+    public ResponseEntity<?> returnIMAGE(@RequestParam int size, @RequestParam String type) {
+        Map<String, String> response = new HashMap<>();
+
+        if (size > 500 || size < 100) {
+            response.put("error", "Image size must be between 100 and 500 pixels");
+            return ResponseEntity.badRequest().body(response);
+        }
+
+        if (!type.matches("(png|jpeg|gif)")) {
+            response.put("error", "Only png, jpeg and gif image types are supported");
+            return ResponseEntity.badRequest().body(response);
+        }
+
+        BufferedImage image = createImage(size);
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType("image/"+type)).body(image);
     }
 
-    private static BufferedImage createImage(Color color) {
-        BufferedImage image = new BufferedImage(750, 750, BufferedImage.TYPE_INT_RGB);
+    private static BufferedImage createImage(int size) {
+        BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
         Graphics2D g = image.createGraphics();
-        g.setColor(color);
-        g.fillRect(250, 250, 250, 250);
+        g.setColor(Color.CYAN);
+        g.fillRect(0, 0, size, size);
 
         return image;
     }
