36afd486d3e7b5e020fda34145cb85ce0f1bdb52
Ro1and19 <leonard6090@gmail.com>
2025-05-19 10:37:34 +0300

    Implemented error correction

--- QRCode Service/src/main/java/org/example/Controller.java
+++ QRCode Service/src/main/java/org/example/Controller.java
@@ -1,16 +1,19 @@
 package org.example;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -26,8 +29,9 @@ public class Controller {
     }
 
     @GetMapping("/api/qrcode")
-    public ResponseEntity<?> returnIMAGE(@RequestParam int size,
-                                         @RequestParam String type,
+    public ResponseEntity<?> returnIMAGE(@RequestParam(defaultValue = "250") int size,
+                                         @RequestParam(defaultValue = "png") String type,
+                                         @RequestParam(defaultValue = "L") String correction,
                                          @RequestParam String contents) {
         Map<String, String> response = new HashMap<>();
 
@@ -41,21 +45,27 @@ public class Controller {
             return ResponseEntity.badRequest().body(response);
         }
 
+        if (!correction.matches("([LMQH])")) {
+            response.put("error", "Permitted error correction levels are L, M, Q, H");
+            return ResponseEntity.badRequest().body(response);
+        }
+
         if (!type.matches("(png|jpeg|gif)")) {
             response.put("error", "Only png, jpeg and gif image types are supported");
             return ResponseEntity.badRequest().body(response);
         }
 
-        BufferedImage image = createImage(size, contents);
+        BufferedImage image = createImage(size, correction,contents);
         return ResponseEntity.ok().contentType(MediaType.parseMediaType("image/"+type)).body(image);
     }
 
-    private static BufferedImage createImage(int size, String contents) {
+    private static BufferedImage createImage(int size, String correction,String contents) {
 
         BufferedImage image = null;
         QRCodeWriter writer = new QRCodeWriter();
+        Map<EncodeHintType, ?> hints = Map.of(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.valueOf(correction));
         try {
-            BitMatrix bitMatrix = writer.encode(contents, BarcodeFormat.QR_CODE, size, size);
+            BitMatrix bitMatrix = writer.encode(contents, BarcodeFormat.QR_CODE, size, size, hints);
             image = MatrixToImageWriter.toBufferedImage(bitMatrix);
         } catch (WriterException e) {
             e.printStackTrace();
